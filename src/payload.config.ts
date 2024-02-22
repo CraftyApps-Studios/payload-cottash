6cf8065cfbbf1b8807667488f3fad15c6998f311
Diego Carbajal <dicornyt@gmail.com>
2024-02-22 18:15:31 -0500

    [MOD] config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,10 @@
-import path from 'path'
+import path from "path";
 
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import { buildConfig } from 'payload/config'
+import { payloadCloud } from "@payloadcms/plugin-cloud";
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { buildConfig } from "payload/config";
 
 import {
   Users,
@@ -17,28 +17,26 @@ import {
 } from "./collections";
 
 export default buildConfig({
-  serverURL: 'https://payload-cottash.up.railway.app',
+  serverURL: "https://payload-cottash.up.railway.app",
   admin: {
     user: Users.slug,
     bundler: webpackBundler(),
   },
   editor: slateEditor({}),
-  cors: ['https://cottash-front.vercel.app'],
-  csrf: ['https://cottash-front.vercel.app'],
-  collections: [
-    Users,
-    Packaging,
-    Textil,
-    Color,
-    Supplier,
-    Ubication,
-    Items,
-  ],
+  cors: [
+    "https://cottash-front.vercel.app" || "",
+    "https://payload-cottash.up.railway.app" || "",
+  ].filter(Boolean),
+  csrf: [
+    "https://cottash-front.vercel.app" || "",
+    "https://payload-cottash.up.railway.app" || "",
+  ].filter(Boolean),
+  collections: [Users, Packaging, Textil, Color, Supplier, Ubication, Items],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
   plugins: [payloadCloud()],
   db: postgresAdapter({
@@ -46,4 +44,4 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URI,
     },
   }),
-})
+});
