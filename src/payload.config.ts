fbb5db03d100052dc7a44796f33d576f6acf9915
Diego Carbajal <dicornyt@gmail.com>
2024-02-22 16:07:36 -0500

    [ADD] serverURL

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -17,6 +17,7 @@ import {
 } from "./collections";
 
 export default buildConfig({
+  serverURL: 'https://payload-cottash.up.railway.app/',
   admin: {
     user: Users.slug,
     bundler: webpackBundler(),
