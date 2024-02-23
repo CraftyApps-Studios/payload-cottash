beeb71e3f8f583a51aae2d52110f2eec85a0d610
Diego Carbajal <dicornyt@gmail.com>
2024-02-23 15:52:29 -0500

    [MOD] cors y csrf

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -23,14 +23,8 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: slateEditor({}),
-  cors: [
-    "https://cottash-front.vercel.app",
-    "https://payload-cottash.up.railway.app",
-  ],
-  csrf: [
-    "https://cottash-front.vercel.app",
-    "https://payload-cottash.up.railway.app",
-  ],
+  cors: ["https://cottash-front.vercel.app"],
+  csrf: ["https://cottash-front.vercel.app"],
   collections: [Users, Packaging, Textil, Color, Supplier, Ubication, Items],
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
