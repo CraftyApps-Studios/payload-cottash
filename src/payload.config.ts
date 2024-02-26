107dd3b7414a9d2fb503c29f843a53b97f13e070
Diego Carbajal <dicornyt@gmail.com>
2024-02-26 18:42:53 -0500

    Revert "[DEL] graphQL"
    This reverts commit 7eff276b5da28aff4ba9fe164b2476bfc7493b18.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -35,6 +35,9 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+  },
   plugins: [payloadCloud()],
   db: postgresAdapter({
     pool: {
