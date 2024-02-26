7eff276b5da28aff4ba9fe164b2476bfc7493b18
Diego Carbajal <dicornyt@gmail.com>
2024-02-26 16:41:33 -0500

    [DEL] graphQL

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -35,9 +35,6 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
-  },
   plugins: [payloadCloud()],
   db: postgresAdapter({
     pool: {
